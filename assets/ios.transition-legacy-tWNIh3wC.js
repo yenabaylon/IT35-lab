d8753d9f48931928ff715698ed867c629c125214
yenabalyon <yenabaylon7@gmail.com>
2025-04-04 09:51:09 +0800

    Updates

--- assets/ios.transition-legacy-tWNIh3wC.js
+++ assets/ios.transition-legacy-tWNIh3wC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BkCSZ2LL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-CFdeyyYY.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
