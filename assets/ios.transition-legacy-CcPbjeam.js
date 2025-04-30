6757828f043760c6fdda56d342c915c949688f42
yenabaylon <yenabaylon7@gmail.com>
2025-04-30 19:46:48 +0800

    Updates

--- assets/ios.transition-legacy-CcPbjeam.js
+++ assets/ios.transition-legacy-CcPbjeam.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CFdeyyYY.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BG1qNe9T.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Ce4oDEtG.js
+++ assets/swipe-back-legacy-Ce4oDEtG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CFdeyyYY.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BG1qNe9T.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
