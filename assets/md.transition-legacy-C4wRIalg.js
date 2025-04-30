dcb0ae07041487b5f666d1a7b44abded8ead0897
yenabaylon <yenabaylon7@gmail.com>
2025-04-30 19:55:58 +0800

    Updates

--- assets/md.transition-legacy-C4wRIalg.js
+++ assets/md.transition-legacy-C4wRIalg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-yhNxIOMk.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BC4SJBw_.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
